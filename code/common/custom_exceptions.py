a709aa39f771826b9f03e1fe0eed0d41ba64166a
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2023-04-13 13:47:32 +0100

    Wrong format exception added, write_df_to_s3 method written and tested

--- code/common/custom_exceptions.py
+++ code/common/custom_exceptions.py
@@ -1,3 +1,11 @@
 """
 Custom Exceptions
 """
+
+class WrongFormatException(Exception):
+    """
+    WrongFormatException class
+
+    Exception that can be raised when the format type
+    given as an arguement is not supported
+    """
--- code/common/s3.py
+++ code/common/s3.py
@@ -2,11 +2,17 @@
 import os
 import logging
 import boto3
+import pandas as pd
+from io import StringIO, BytesIO
+from code.common.custom_exceptions import WrongFormatException
+from code.common.constants import S3FileTypes
+
 
 class S3BucketConnector():
     """
     Class for interacting with S3 Buckets
     """
+
     def __init__(self, access_key: str, secret_key: str, endpoint_url: str, bucket: str):
         """
         Constructor for S3BucketConnector
@@ -21,7 +27,8 @@ class S3BucketConnector():
         self.endpoint_url = endpoint_url
         self.session = boto3.Session(aws_access_key_id=os.environ[access_key],
                                      aws_secret_access_key=os.environ[secret_key])
-        self._s3 = self.session.resource(service_name='s3', endpoint_url=endpoint_url)
+        self._s3 = self.session.resource(
+            service_name='s3', endpoint_url=endpoint_url)
         self._bucket = self._s3.Bucket(bucket)
 
     def list_files_in_prefix(self, prefix: str):
@@ -35,9 +42,58 @@ class S3BucketConnector():
         files = [obj.key for obj in self._bucket.objects.filter(Prefix=prefix)]
         return files
 
-    def read_csv_to_df(self):
-        pass
+    def read_csv_to_df(self, key: str, encoding: str = 'utf-8', sep: str = ','):
+        """
+        Reading a csv file from the S3 bucket and returning a dataframe
 
-    def write_df_to_s3(self):
-        pass
-    
+        :param key: key of the file that should be read
+        :encoding: encodeing of the date inside the csv file
+        :sep: seperator of the csv file
+
+        returns:
+          data_frame: Pandas DataFrame containing the data of the csv file
+        """
+        self._logger.info("Reading file %s/%s/%s",
+                          self.endpoint_url, self._bucket.name, key)
+        csv_obj = self._bucket.Object(key=key).get().get(
+            'Body').read().decode(encoding)
+        data = StringIO(csv_obj)
+        data_frame = pd.read_csv(data, sep=sep)
+        return data_frame
+
+    def write_df_to_s3(self, data_frame: pd.DataFrame, key: str, file_format: str):
+        """
+        Writing a Pandas dataframe to S3
+        supported formats: .csv, .parquet
+
+        :param data_frame: Pandas DataFrame that should be written
+        :param key: target key of the saved file
+        :param file_format: format of the saved file
+        """
+        if data_frame.empty:
+            self._logger.info(
+                'The dataframe is empty. No file will be written')
+            return None
+        if file_format == S3FileTypes.CSV.value:
+            out_buffer = StringIO()
+            data_frame.to_csv(out_buffer, index=False)
+            return self.__put_object(out_buffer, key)
+        if file_format == S3FileTypes.PARQUET.value:
+            out_buffer = BytesIO()
+            data_frame.to_parquet(out_buffer, index=False)
+            return self.__put_object(out_buffer, key)
+        self._logger.info(
+            'The file format %s is not supported for writing to S3', file_format)
+        raise WrongFormatException
+
+    def __put_object(self, out_buffer: StringIO or BytesIO, key: str):
+        """
+        Helper function for self.write_df_to_s3()
+
+        :out_buffer: StringIO | BytesIO that should be written
+        :key: target key of the saved file
+        """
+        self._logger.info('Writing file to %s/%s/%s',
+                          self.endpoint_url, self._bucket.name, key)
+        self._bucket.put_object(Body=out_buffer.getvalue(), Key=key)
+        return True
